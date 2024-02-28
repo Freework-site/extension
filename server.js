4826e1c25849d7f84c95641c6b6b057fa624e3f8
WebCreator <72634873+Freework-site@users.noreply.github.com>
2024-02-28 00:51:00 -0700

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res) => {
-    res.send('Hello')
+    res.send('Hello form github push new update')
 })
 
 app.listen(PORT, () => {
