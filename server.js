83ab915789cbbff9ce0690e7242fbf50071b5169
WebCreator <72634873+Freework-site@users.noreply.github.com>
2024-02-28 01:18:13 -0700

    Testing

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require("express")
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hello form github push new update')
-})
+app.use(express.json());
+app.use(cors()); // Add this line to enable CORS
 
+// Endpoint to receive data from the frontend
+
+app.post('/', (req,res) => {
+    res.render("Hello")
+})
+app.post('/data', (req, res) => {
+    console.log('Data received:', req.body);
+    res.send('Data received successfully');
+});
+
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
